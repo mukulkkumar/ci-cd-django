fcf598c952b406632c3864af8e00755d4eb6775d
mukulkkumar <mukulkkumarr@gmail.com>
2021-12-13 09:39:40 +0530

    Changes in admin for student app

--- student/admin.py
+++ student/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Student
 
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'reg_number', 'date_of_admission')
+
 # Register your models here.
-admin.site.register(Student)
+admin.site.register(Student, StudentAdmin)
