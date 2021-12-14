32584016b244a7175b0acecf14d7edf07b8852bc
mukulkkumar <mukulkkumarr@gmail.com>
2021-12-14 09:59:00 +0530

    Changes in build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,4 +4,4 @@ echo "****************************"
 echo "** Building Docker Image ***"
 echo "****************************"
 
-cd ../ && sudo docker-compose build --no-cache
+cd ../ && docker-compose build --no-cache
