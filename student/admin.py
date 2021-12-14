1c38f8fc9a4813acc2ca127d2158d7ef02a1c5b9
mukulkkumar <mukulkkumarr@gmail.com>
2021-12-14 09:16:53 +0530

    Changes in student admin

--- student/admin.py
+++ student/admin.py
@@ -4,5 +4,4 @@ from .models import Student
 class StudentAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'reg_number', 'date_of_admission')
 
-# Register your models here.
 admin.site.register(Student, StudentAdmin)
