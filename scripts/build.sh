0e587c02f17186305acfdb85d6f5fbf650f3ac91
mukulkkumar <mukulkkumarr@gmail.com>
2021-12-14 09:41:14 +0530

    Changes in build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,4 +4,4 @@ echo "****************************"
 echo "** Building Docker Image ***"
 echo "****************************"
 
-cd /home/ubuntu/project/ci-cd-django/ && sudo docker-compose build --no-cache
+sudo docker-compose build --no-cache
