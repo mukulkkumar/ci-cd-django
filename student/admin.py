cf288352177f598d968542cf99b2f3189ffe8c7e
mukulkkumar <mukulkkumarr@gmail.com>
2021-12-14 10:15:15 +0530

    Added comment in admin file

--- student/admin.py
+++ student/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Student
 
+# Student admin class
 class StudentAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'reg_number', 'date_of_admission')
 
