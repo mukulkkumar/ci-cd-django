094e6cbb6e8f55704a90d766c517ab684a07665a
mukulkkumar <mukulkkumarr@gmail.com>
2021-12-15 13:47:05 +0530

    Changes in script

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,4 +5,4 @@ echo "** Building Docker Image ***"
 echo "****************************"
 
 cd /home/
-ssh -i zookeeper_solr.pem ubuntu@3.137.101.239 sudo docker-compose -f /home/ubuntu/project/ci-cd-django/docker-compose.yml build --no-cache
+ssh -i zookeeper_solr.pem ubuntu@3.140.100.129 sudo docker-compose -f /home/ubuntu/project/ci-cd-django/docker-compose.yml build --no-cache
