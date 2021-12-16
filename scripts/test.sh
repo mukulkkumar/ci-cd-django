12b72c5c94c693d4c4f80e0dd7565544ba46f4c1
mukulk-tml <mukulk.ttl@tatamotors.com>
2021-12-16 21:26:07 +0530

    Script Changes

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,9 +4,9 @@ echo "**********************"
 echo "**Test Docker Image **"
 echo "**********************"
 
-# ssh -i /home/zookeeper_solr.pem ubuntu@3.23.2.101 sudo docker run --name django_server -d -p 8000:8000 django_server:latest
+ssh -i /home/zookeeper_solr.pem ubuntu@3.137.101.239 sudo docker run --name django_server -d -p 8000:8000 django_server:latest
 
-if [[ $(ssh -i /home/zookeeper_solr.pem ubuntu@3.23.2.101 sudo docker exec -t django_server bash -c "python manage.py test" | grep "OK") ]]; then
+if [[ $(ssh -i /home/zookeeper_solr.pem ubuntu@3.137.101.239 sudo docker exec -t django_server bash -c "python manage.py test" | grep "OK") ]]; then
     echo "Test successful"
 else
     echo "Test Failure"
