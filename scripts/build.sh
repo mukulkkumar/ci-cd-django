34c9459a30eb2eef508131bd3cdab5f394d6ab5d
mukulkkumar <mukulkkumarr@gmail.com>
2021-12-16 22:53:04 +0530

    Changes in script

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,4 +4,4 @@ echo "****************************"
 echo "** Building Docker Image ***"
 echo "****************************"
 
-# ssh -i /home/zookeeper_solr.pem ubuntu@3.137.101.239 sudo docker-compose -f /home/ubuntu/project/ci-cd-django/docker-compose.yml build --no-cache
+ssh -i /home/zookeeper_solr.pem ubuntu@3.137.101.239 sudo docker-compose -f /home/ubuntu/project/ci-cd-django/docker-compose.yml build --no-cache
--- scripts/test.sh
+++ scripts/test.sh
@@ -4,7 +4,7 @@ echo "**********************"
 echo "**Test Docker Image **"
 echo "**********************"
 
-# ssh -i /home/zookeeper_solr.pem ubuntu@3.137.101.239 sudo docker run --name django_server -d -p 8000:8000 django_server:latest
+ssh -i /home/zookeeper_solr.pem ubuntu@3.137.101.239 sudo docker run --name django_server -d -p 8000:8000 django_server:latest
 
 if ssh -i /home/zookeeper_solr.pem ubuntu@3.137.101.239 sudo docker-compose -f /home/ubuntu/project/ci-cd-django/docker-compose.yml run web python manage.py test; then
     echo "Test successful"
