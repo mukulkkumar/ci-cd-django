4a13fc4205b70484d53e79a299f41c151810c76e
mukulkkumar <mukulkkumarr@gmail.com>
2021-12-18 21:06:26 +0530

    Changes in script

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,9 +4,9 @@ echo "**********************"
 echo "**Test Docker Image **"
 echo "**********************"
 
-ssh -i /home/zookeeper_solr.pem ubuntu@18.223.29.83 sudo docker run --name django_server -d -p 8000:8000 django_server:latest
+ssh -i /home/devops.pem ubuntu@18.223.29.83 sudo docker run --name django_server -d -p 8000:8000 django_server:latest
 
-if ssh -i /home/zookeeper_solr.pem ubuntu@18.223.29.83 sudo docker-compose -f /home/ubuntu/project/ci-cd-django/docker-compose.yml run web python manage.py test; then
+if ssh -i /home/devops.pem ubuntu@18.223.29.83 sudo docker-compose -f /home/ubuntu/project/ci-cd-django/docker-compose.yml run web python manage.py test; then
     echo "Test successful"
 else
     echo "Test Failure"
