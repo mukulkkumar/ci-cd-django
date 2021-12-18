7562d5273ea1c6fbaabb05800780cf20661acf44
mukulkkumar <mukulkkumarr@gmail.com>
2021-12-18 21:01:10 +0530

    changes in scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,5 +4,5 @@ echo "**********************"
 echo "**Deploy Docker Image **"
 echo "**********************"
 
-ssh -i /home/zookeeper_solr.pem ubuntu@3.137.101.234 sudo docker pull django_server:lastest
-ssh -i /home/zookeeper_solr.pem ubuntu@3.137.101.234 sudo docker run --name django_server -d -p 8000:8000 django_server:latest
+# ssh -i /home/zookeeper_solr.pem ubuntu@3.137.101.234 sudo docker pull django_server:lastest
+# ssh -i /home/zookeeper_solr.pem ubuntu@3.137.101.234 sudo docker run --name django_server -d -p 8000:8000 django_server:latest
--- scripts/push.sh
+++ scripts/push.sh
@@ -4,11 +4,11 @@ echo "********************"
 echo "** Pushing image ***"
 echo "********************"
 
-IMAGE="django_server"
+# IMAGE="django_server"
 
-echo "** Logging in ***"
-docker login -u kumarm5 -p $PASS
-echo "*** Tagging image ***"
-docker tag $IMAGE:$BUILD_TAG kumarm5/$IMAGE:$BUILD_TAG
-echo "*** Pushing image ***"
-docker push kumarm5/$IMAGE:$BUILD_TAG
+# echo "** Logging in ***"
+# docker login -u kumarm5 -p $PASS
+# echo "*** Tagging image ***"
+# docker tag $IMAGE:$BUILD_TAG kumarm5/$IMAGE:$BUILD_TAG
+# echo "*** Pushing image ***"
+# docker push kumarm5/$IMAGE:$BUILD_TAG
