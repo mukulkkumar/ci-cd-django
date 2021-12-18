47bc7f4d87a5d9e9990d9ffd168a65fb63535f20
mukulkkumar <mukulkkumarr@gmail.com>
2021-12-18 22:36:58 +0530

    test script changes

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,7 +6,7 @@ echo "**********************"
 
 # ssh -i /home/devops.pem ubuntu@18.118.213.29 sudo docker run --name django_server -d -p 8000:8000 django_server:latest
 
-if ssh -i /home/devops.pem ubuntu@18.118.213.29 sudo docker-compose -f /home/ubuntu/project/ci-cd-django/docker-compose.yml run web python manage.py test; then
+if ssh -i /home/devops.pem ubuntu@3.131.160.189 sudo docker-compose -f /home/ubuntu/project/ci-cd-django/docker-compose.yml run web python manage.py test; then
     echo "Test successful"
 else
     echo "Test Failure"
