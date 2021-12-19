96a00243ce2285d846bbf202bdfeae393d826448
mukulkkumar <mukulkkumarr@gmail.com>
2021-12-19 17:21:07 +0530

    push script changes for tag

--- scripts/push.sh
+++ scripts/push.sh
@@ -9,8 +9,8 @@ IMAGE="django_server"
 echo "** Logging in ***"
 ssh -i /home/devops.pem ubuntu@3.18.64.78 sudo docker login -u kumarm5 -p $PASS
 echo "*** Tagging image ***"
-ssh -i /home/devops.pem ubuntu@3.18.64.78 sudo docker tag $IMAGE:$BUILD_TAG kumarm5/$IMAGE:$BUILD_TAG
+ssh -i /home/devops.pem ubuntu@3.18.64.78 sudo docker tag $IMAGE:latest kumarm5/$IMAGE:latest
 echo "*** Pushing image ***"
-ssh -i /home/devops.pem ubuntu@3.18.64.78 sudo docker push kumarm5/$IMAGE:$BUILD_TAG
+ssh -i /home/devops.pem ubuntu@3.18.64.78 sudo docker push kumarm5/$IMAGE:latest
 echo "** Logout ***"
 ssh -i /home/devops.pem ubuntu@3.18.64.78 sudo docker logout
