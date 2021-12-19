04ee329cb44886d05852968f7c79e59efc17e946
mukulkkumar <mukulkkumarr@gmail.com>
2021-12-19 17:16:21 +0530

    push script changes

--- scripts/push.sh
+++ scripts/push.sh
@@ -6,12 +6,11 @@ echo "********************"
 
 IMAGE="django_server"
 
-# echo "** Logging in ***"
-# docker login -u kumarm5 -p $PASS
-# echo "*** Tagging image ***"
-# docker tag $IMAGE:$BUILD_TAG kumarm5/$IMAGE:$BUILD_TAG
-# echo "*** Pushing image ***"
-# docker push kumarm5/$IMAGE:$BUILD_TAG
-
-echo "Build Tag"
-echo $BUILD_TAG
+echo "** Logging in ***"
+ssh -i /home/devops.pem ubuntu@3.18.64.78 sudo docker login -u kumarm5 -p $PASS
+echo "*** Tagging image ***"
+ssh -i /home/devops.pem ubuntu@3.18.64.78 sudo docker tag $IMAGE:$BUILD_TAG kumarm5/$IMAGE:$BUILD_TAG
+echo "*** Pushing image ***"
+ssh -i /home/devops.pem ubuntu@3.18.64.78 sudo docker push kumarm5/$IMAGE:$BUILD_TAG
+echo "** Logout ***"
+ssh -i /home/devops.pem ubuntu@3.18.64.78 sudo docker logout
